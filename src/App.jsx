2b5fd853d5d687b550dece9d993711e68a9d6db3
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2024-07-28 18:16:22 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,31 @@ import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactForm from "./components/ContactForm/ContactForm";
 import css from "./App.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchContacts } from "./redux/contacts/contactsApi";
+import { selectError, selectLoading } from "./redux/contacts/contactsSlice";
+import Loader from "./components/Loader/Loader";
+import Error from "./components/Error/Error";
 
 function App() {
   const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
+
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
+
   return (
-    < div className={css.container}>
+    <div className={css.container}>
       <h1 className={css.pageTitle}>Phonebook</h1>
       <ContactForm />
       <SearchBox />
+      {loading && <Loader />}
+      {isError && <Error />}
       <ContactList />
-    </ div>
+    </div>
   );
 }
 
